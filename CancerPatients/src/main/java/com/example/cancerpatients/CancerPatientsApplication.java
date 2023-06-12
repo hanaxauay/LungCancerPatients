99d116881e54c7121b1351e2c0d65c74f6d330ff
hanaxauay <hana0_0aa@naver.com>
2023-06-13 02:20:22 +0900

    first commit

---
+++ CancerPatients/src/main/java/com/example/cancerpatients/CancerPatientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cancerpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancerPatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancerPatientsApplication.class, args);
+	}
+
+}
---
+++ CancerPatients/src/test/java/com/example/cancerpatients/CancerPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cancerpatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancerPatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
