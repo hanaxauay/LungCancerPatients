6282a86e9a684f3778302009f49282885b7af890
hanaxauay <hana0_0aa@naver.com>
2023-06-21 01:48:46 +0900

    인사말 완성

--- CancerPatients/src/main/java/com/example/cancerpatients/controller/HomeController.java
+++ CancerPatients/src/main/java/com/example/cancerpatients/controller/HomeController.java
@@ -7,8 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-//    @GetMapping("/")
-//    public String home(){
-//        return "index.html";
-//    }
+    @GetMapping("/")
+    public String home(){
+        return "index.html";
+    }
+
+    @GetMapping("/organization")
+    public String goOrganization(){
+        return "organization.html";
+    }
+    @GetMapping("/greeting")
+    public String goGreeting(){
+        return "greeting.html";
+    }
 }
