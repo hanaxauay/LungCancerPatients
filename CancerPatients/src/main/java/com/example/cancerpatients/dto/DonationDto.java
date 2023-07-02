07860e0a9fa66fb939e3c84c7c4ab04030166139
hanaxauay <hana0_0aa@naver.com>
2023-07-02 23:54:43 +0900

    .

--- CancerPatients/src/main/java/com/example/cancerpatients/dto/DonationDto.java
+++ CancerPatients/src/main/java/com/example/cancerpatients/dto/DonationDto.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Entity
+
 @Table(name="DONATION")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
 
 public class DonationDto {
+    @Id
+    @Column(name="SEQ",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본 키 생성을 데이터베이스에 위임->즉, id 값을 null로 하면 DB가 알아서 AUTO_INCREMENT 해준다.
+    private Long seq;
+
+    private String title;
+
+    private String content;
+
+    private String author;
+
+    private LocalDat
+    private int views;
+
+
 }
