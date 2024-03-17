e4b92d1a144eead094bf23950ba51fac36f2dae0
hanaxauay <100827911+hanaxauay@users.noreply.github.com>
2024-03-18 01:53:28 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ else
 fi
 
 cd /home/ec2-user/app
-sudo docker build -t tcat-api-spring-boot-docker .
-sudo docker run -d -p 8080:8080 tcat-api-spring-boot-docker
+sudo docker build -t lung-cancer-patients-api-spring-boot-docker .
+sudo docker run -d -p 8080:8080 lung-cancer-patients-api-spring-boot-docker
