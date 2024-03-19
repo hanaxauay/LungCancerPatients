12a8036661fc6b3d51faff69f98579b4ea44b9ad
hanaxauay <hana0_0aa@naver.com>
2024-03-20 03:23:04 +0900

    겁나고침

--- build/resources/main/static/js/index.js
+++ build/resources/main/static/js/index.js
@@ -6,3 +6,12 @@ $(document).ready(function(){
         autoplaySpeed: 1000,
     });
 });
+
+$(document).ready(function(){
+    $('.autoplay_sponsor').slick({
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
